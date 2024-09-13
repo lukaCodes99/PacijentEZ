f2bfcd8845be38e59869fe853bc54574d53a7f9b
Luka <126253179+lukaCodes99@users.noreply.github.com>
2024-09-13 14:17:25 +0200

    Update PatientTreatmentMapper.java

--- src/main/java/com/maletic/pacijentez/mapper/PatientTreatmentMapper.java
+++ src/main/java/com/maletic/pacijentez/mapper/PatientTreatmentMapper.java
@@ -1,24 +1,5 @@
 package com.maletic.pacijentez.mapper;
 
-import com.maletic.pacijentez.dto.PatientTreatmentDTO;
-import com.maletic.pacijentez.model.PatientTreatment;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Component;
-
-@Component
 public class PatientTreatmentMapper {
 
-    private ModelMapper modelMapper;
-
-    public PatientTreatmentMapper() {
-        this.modelMapper = new ModelMapper();
-    }
-
-    public PatientTreatmentDTO mapPTtoPTdto(PatientTreatment patientTreatment) {
-        return modelMapper.map(patientTreatment, PatientTreatmentDTO.class);
-    }
-
-    public PatientTreatment mapPTdtoToPT(PatientTreatmentDTO patientTreatmentDTO) {
-        return modelMapper.map(patientTreatmentDTO, PatientTreatment.class);
-    }
 }
