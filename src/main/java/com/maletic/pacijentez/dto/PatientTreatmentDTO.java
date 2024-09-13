113420b5edfbe33d3d611a191ba1d517023880c6
Luka <126253179+lukaCodes99@users.noreply.github.com>
2024-09-13 14:17:06 +0200

    Update PatientTreatmentDTO

--- src/main/java/com/maletic/pacijentez/dto/PatientTreatmentDTO.java
+++ src/main/java/com/maletic/pacijentez/dto/PatientTreatmentDTO.java
@@ -1,21 +1,5 @@
 package com.maletic.pacijentez.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.*;
-
-import java.time.LocalDateTime;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class PatientTreatmentDTO {
-
-    private Integer id;
-    private Integer patientId;
-    private Integer treatmentId;
-    private Integer inserterId;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime insertedAt;
-
 
 }
