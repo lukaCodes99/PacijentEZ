051cc73c147a7bd87368322ca49898c40c41857e
Luka <126253179+lukaCodes99@users.noreply.github.com>
2024-09-13 14:17:43 +0200

    Update PatientTreatmentRepository.java

--- src/main/java/com/maletic/pacijentez/repository/PatientTreatmentRepository.java
+++ src/main/java/com/maletic/pacijentez/repository/PatientTreatmentRepository.java
@@ -1,20 +1,5 @@
 package com.maletic.pacijentez.repository;
 
-import com.maletic.pacijentez.model.PatientTreatment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
-
-@Repository
 public interface PatientTreatmentRepository extends JpaRepository<PatientTreatment, Integer> {
 
-    @Query("SELECT pt FROM PatientTreatment pt " +
-            " JOIN FETCH pt.patientId p" +
-            " JOIN FETCH pt.treatmentId t" +
-            " JOIN FETCH pt.inserterId i"
-    )
-    List<PatientTreatment> findAll();
 }
